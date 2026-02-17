14d8c7d52cc7492121db8a38892172e2b6ff4e05
Ufuk Düşovalı <ufuk_best@hotmail.com>
2026-02-17 12:40:29 +0300

    About CQRS bitti

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Commands/AboutCommands/RemoveAboutCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyCarBook.Application.Features.CQRS.Commands.AboutCommands
+{
+    public class RemoveAboutCommand
+    {
+        public RemoveAboutCommand(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+    }
+}
---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyCarBook.Application.Features.CQRS.Results.AboutResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        
+        public async Task<List<GetAboutQueryResult>>Handle()
+        {
+            var values=await _repository.GetAllAsync();
+            return values.Select(x => new GetAboutQueryResult
+            {
+                AboutID = x.AboutID,
+                Title = x.Title,
+                Description = x.Description,
+                ImageUrl = x.ImageUrl
+            }).ToList();
+        }
+    }
+}
