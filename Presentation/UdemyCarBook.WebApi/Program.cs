2e6301cd6701a3ec2118fcd381002d0bc6adc535
Ufuk Düşovalı <ufuk_best@hotmail.com>
2026-02-22 13:10:09 +0300

    about api yazımı ve testi bitti

--- Presentation/UdemyCarBook.WebApi/Program.cs
+++ Presentation/UdemyCarBook.WebApi/Program.cs
@@ -1,10 +1,29 @@
+using Microsoft.Data.SqlClient;
+using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using UdemyCarBook.Application.Features.CQRS.Results.AboutResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Persistence.Context;
+using UdemyCarBook.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<CarBookContext>();
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
+// Register the query handler types (previously the *Result types were registered by mistake)
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +31,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapGet("/", () => Results.Redirect("/swagger"));
 }
 
 app.UseHttpsRedirection();
